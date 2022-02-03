642a4305f6d95966ee86839f3ef2995547c3d065
Simon Katz <sk@nomistech.com>
2022-02-03 13:12:01 +0000

    Add some `unused-public-var` declarations

--- dev/dev.clj
+++ dev/dev.clj
@@ -4,13 +4,16 @@
    [other-code.other-a.other-b.other-c.other-d.other-e :as other-e]
    [our-code.our-a.our-b.our-c.our-d.our-e :as our-e]))
 
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defn log-1 []
   (other-e/log-1)
   (our-e/log-1))
 
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defn log-forever [interval-ms]
   (future (other-e/log-forever interval-ms))
   (future (our-e/log-forever interval-ms)))
 
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defn log-from-dev []
   (log/info "DevDevDev"))
--- dev/user.clj
+++ dev/user.clj
@@ -1,5 +1,6 @@
 (ns user)
 
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (defn dev []
   (require 'dev)
   (in-ns 'dev))

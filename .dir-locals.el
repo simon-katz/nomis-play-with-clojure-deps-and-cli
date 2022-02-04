b8f39f2830a2083fd6688633f8d0b2c79f3f1502
Simon Katz <sk@nomistech.com>
2022-02-04 16:36:13 +0000

    Change .dir-locals.el to only do things for `clojure-mode`

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,2 @@
-((nil
-  (cider-clojure-cli-aliases . "project/dev:user/dev") ; c.f. the `clj-dev.sh` script.
-  ))
+((clojure-mode . ((cider-clojure-cli-aliases . "project/dev:user/dev") ; c.f. the `clj-dev.sh` script.
+                  )))

5074fe82151d9ed75870defc16e3d2873a462ca3
Simon Katz <sk@nomistech.com>
2022-12-27 12:24:01 +0000

    Remove unnecessary `[]`

--- test/our_code/temp_test.clj
+++ test/our_code/temp_test.clj
@@ -3,7 +3,7 @@
    [clojure.test :refer [deftest is]]
    [our-code.temp :as sut]))
 
-(deftest de-morgan-test []
+(deftest de-morgan-test
   (doseq [x [false true]
           y [false true]]
     (is (= (sut/foo-1 x y)

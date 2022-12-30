28888e86ea48dc990a028577a51a1f2ae5bca751
Simon Katz <sk@nomistech.com>
2022-12-30 11:56:40 +0000

    Add more tests so that you can see autotest progress bar better

--- test/our_code/ns_002_test.clj
+++ test/our_code/ns_002_test.clj
@@ -6,3 +6,15 @@
 (deftest some-test-001
   (is (= 42
          sut/foo)))
+
+(deftest some-test-002
+  (is (= 42
+         sut/foo)))
+
+(deftest some-test-003
+  (is (= 42
+         sut/foo)))
+
+(deftest some-test-004
+  (is (= 42
+         sut/foo)))

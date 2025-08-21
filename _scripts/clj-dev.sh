ef4ae1a2d7647621b3bbed4d388bb0d8fd38a8d0
Simon Katz <sk@nomistech.com>
2025-08-21 19:31:47 +0100

    Hack clj-dev.sh because of new versions

--- _scripts/clj-dev.sh
+++ _scripts/clj-dev.sh
@@ -2,4 +2,8 @@
 
 # c.f. `cider-clojure-cli-aliases` in .dir-locals.clj.
 
-clj -M:project/dev:user/dev:user/cider-nrepl
+# clj -M:project/dev:user/dev:user/cider-nrepl
+
+echo 'We have gotten rid of the `:user/cider-nrepl` alias. Start a REPL from Emacs and then copy the startup command to here.'
+
+exit 1
